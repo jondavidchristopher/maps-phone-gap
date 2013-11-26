f8e81db8e0fcf388a97afedd765104bbe824c7d2
Jon Christopher <jon@rubbertreestudios.com>
2013-11-26 15:43:37 -0800

    Login Done moving on to FB

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -2,13 +2,25 @@ $(function() {
     $(document).bind("deviceready", function() {
         App = Ember.Application.create();
 
+        // Models
+        // Placeholder for environments
+        App.Environment = Ember.Object.extend({
+          // these will be supplied by `create`
+          someVariable: null,
+
+          remoteURL: function() {
+            return this.get('firstName') + ' ' + this.get('lastName');
+          }.property('firstName', 'lastName')
+        });        
+
+        // Routes
         App.Router.map(function() {
             this.resource('joyride');
             this.resource('signin');
             this.resource('signup');
             this.resource('forgot_password');
             this.resource('geolocate');
-            this.resource('geolocate-error');
+            this.resource('geolocate-failure');
             this.resource('bars');
         });
 
@@ -18,10 +30,15 @@ $(function() {
             }
         });
 
+
+        App.SigninRoute = Ember.Route.extend({
+            setupController: function(controller, context) {
+                controller.reset();
+            }
+        });
+
         App.JoyrideRoute = Ember.Route.extend({
-          // model: function() {
-          //   return ['red', 'yellow', 'blue'];
-          // }
+            // Do amazing story here
         });
 
         App.GeolocateRoute = Ember.Route.extend({
@@ -38,22 +55,36 @@ $(function() {
                       'Longitude: '         + position.coords.longitude);
             },
             onError: function(error) {
-                this.transitionTo('geolocate-error');
+                this.transitionTo('geolocate-failure');
             }
         });
 
         App.BarsRoute = Ember.Route.extend({
             model: function() {
-                return new Ember.RSVP.Promise(function(resolve) {
-                    navigator.geolocation.getCurrentPosition(this.onSuccess, this.onError);
-                });
             }          
         });
 
-        App.GeoLocation = Ember.Object.extend({
-          fullName: function() {
-            return this.get('firstName') + ' ' + this.get('lastName');
-          }.property('firstName', 'lastName')
-        });        
+        // Controllers
+        App.SigninController = Ember.Controller.extend({
+            reset: function() {
+                this.setProperties({
+                    username: "",
+                    password: "",
+                    errorMessage: ""
+                });
+            },            
+            signin: function() {
+                var self = this, data = this.getProperties('username','password');
+                // Clear out any error messages.
+                this.set('errorMessage', null);                
+                Ember.$.post('localhost:3000/api/v1/login', data).then(function(response) {
+                    self.set('errorMessage', response.message);
+                    if (response.success) {
+                        self.set('token', response.token);                        
+                    }
+                });
+            }
+        });
+
     });
 });
--- www/js/app.js
+++ www/js/app.js
@@ -2,88 +2,111 @@ $(function() {
     $(document).bind("deviceready", function() {
         App = Ember.Application.create();
 
-        // Models
-        App.Environment = Ember.Object.extend({
-          // these will be supplied by `create`
-          which: null,
-
-          remoteURL: function() {
-            return this.get('firstName') + ' ' + this.get('lastName');
-          }.property('firstName', 'lastName')
-        });        
-
         // Routes
-        App.Router.map(function() {
-            this.resource('joyride');
-            this.resource('signin');
-            this.resource('signup');
-            this.resource('forgot_password');
-            this.resource('geolocate');
-            this.resource('geolocate-failure');
-            this.resource('bars');
-        });
+            App.Router.map(function() {
+                this.resource('joyride');
+                this.resource('signin');
+                this.resource('signup');
+                this.resource('forgot_password');
+                this.resource('geolocate');
+                this.resource('geolocate-failure');
+                this.resource('bars');
+            });
 
-        App.IndexRoute = Ember.Route.extend({
-            beforeModel: function() {
-                this.transitionTo('joyride');
-            }
-        });
+            App.IndexRoute = Ember.Route.extend({
+                beforeModel: function() {
+                    this.transitionTo('joyride');
+                }
+            });
 
 
-        App.SigninRoute = Ember.Route.extend({
-            setupController: function(controller, context) {
-                controller.reset();
-            }
-        });
+            App.JoyrideRoute = Ember.Route.extend({
+                // Do amazing story here
+            });
 
-        App.JoyrideRoute = Ember.Route.extend({
-            // Do amazing story here
-        });
+            App.GeolocateRoute = Ember.Route.extend({
+                // Get current Location      
+                model: function() {
+                    navigator.geolocation.getCurrentPosition(
+                        $.proxy(this.onSuccess, this), 
+                        $.proxy(this.onError, this), 
+                        {timeout: 15000, enableHighAccuracy: true}
+                    );
+                },     
+                onSuccess: function(position) {
+                    alert('Latitude: '          + position.coords.latitude          + '\n' +
+                          'Longitude: '         + position.coords.longitude);
+                },
+                onError: function(error) {
+                    this.transitionTo('geolocate-failure');
+                }
+            });
 
-        App.GeolocateRoute = Ember.Route.extend({
-            // Get current Location      
-            model: function() {
-                navigator.geolocation.getCurrentPosition(
-                    $.proxy(this.onSuccess, this), 
-                    $.proxy(this.onError, this), 
-                    {timeout: 15000, enableHighAccuracy: true}
-                );
-            },     
-            onSuccess: function(position) {
-                alert('Latitude: '          + position.coords.latitude          + '\n' +
-                      'Longitude: '         + position.coords.longitude);
-            },
-            onError: function(error) {
-                this.transitionTo('geolocate-failure');
-            }
-        });
-
-        App.BarsRoute = Ember.Route.extend({
-            model: function() {
-            }          
-        });
+            App.BarsRoute = Ember.Route.extend({
+                model: function() {
+                }          
+            });
 
         // Controllers
-        App.SigninController = Ember.Controller.extend({
-            reset: function() {
-                this.setProperties({
-                    username: "",
-                    password: "",
-                    errorMessage: ""
-                });
-            },            
-            signin: function() {
-                var self = this, data = this.getProperties('username','password');
-                // Clear out any error messages.
-                this.set('errorMessage', null);                
-                Ember.$.post('localhost:3000', data).then(function(response) {
-                    self.set('errorMessage', response.message);
-                    if (response.success) {
-                        self.set('token', response.token);                        
+            App.SigninController = Ember.Controller.extend({
+                signin: function() {
+                    var data = this.getProperties('email','password');
+                    api.call ("login", data, $.proxy(function(response) {
+                        if (response.errors) {
+                            $(".well").addClass("has-error");
+                            this.set('errorMessage', response.errors);                    
+                        } else {
+                            api.set('token', response.token);   
+                            this.transitionTo('geolocate');
+                        }
+                    }, this));
+                }
+            });
+
+        // Service Objects
+           App.Api = Ember.Object.extend({
+                url: null,
+                version: null,
+                token: null,
+
+                call: function(endpoint, data, callback) {
+                    if (this.get("token")) {
+                        'auth_token='+ this.get("token") + '&' + data
                     }
-                });
-            }
-        });
+
+                    $.ajax({
+                        type: "GET",
+                        dataType: "jsonp",
+                        url: this.get("url") + "/api/" + this.get("version") + "/" + endpoint,
+                        cache: false,
+                        timeout: 3000,
+                        data: data,
+
+                        success: callback,
+
+                        error: function(data){
+                            console.log(data);
+                        }
+                    });
+                }
+            });
+
+            App.Environment = Ember.Object.extend({
+            // Placeholder for environments
+              someVariable: null,
+
+              remoteURL: function() {
+                return this.get('firstName') + ' ' + this.get('lastName');
+              }.property('firstName', 'lastName')
+            });  
+
+                   
+
+        // Initialize App
+            var api = App.Api.create({
+              url: "http://localhost:3000",
+              version:  "v1"
+            });       
 
     });
 });
